feef502165671e19aa3f000768491a0946fd0666
Muhammed Abubakr <mabobakr365@gmail.com>
2022-07-21 00:10:00 +0200

    Added the functionality file

---
+++ main.js
@@ -0,0 +1,35 @@
+var dropDownMenu = document.querySelector('.drop-down--menu')
+var darkMode = document.getElementById('dark')
+var root = document.querySelector('.root')
+
+function showDropdownMenu(){
+    dropDownMenu.classList.toggle('drop-down--menu---view')
+}
+
+if (localStorage.getItem("theme") === "light"){
+    darkMode.classList.remove('white')
+    darkMode.classList.remove('black')
+    root.classList.remove('dark-theme')
+}
+else if (localStorage.getItem("theme") === "dark"){
+    darkMode.classList.add('white')
+    darkMode.classList.add('black')
+    root.classList.add('dark-theme')
+}
+else{
+    localStorage.setItem("theme", "light")
+}
+
+function changeColor(){
+    darkMode.classList.toggle('white')
+    darkMode.classList.toggle('black')
+    root.classList.toggle('dark-theme')
+    
+    if (localStorage.getItem("theme") === "light"){
+        localStorage.setItem("theme", "dark")
+    }
+    else{
+        localStorage.setItem("theme", "light")
+    }
+}
+
